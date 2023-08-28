0ec01ef124a6960f39c8438f1bee8f417df72ce9
mahmoudali747 <133993959+mahmoudali747@users.noreply.github.com>
2023-08-28 16:57:19 +0300

    This is test

--- app/index.js
+++ app/index.js
@@ -12,7 +12,7 @@ export default  function App  ({ navigation }){
        navigation.navigate('Premierleague')
 
     }
-
+const navigation =useNavigation();
     return (
             
          <ScrollView>
